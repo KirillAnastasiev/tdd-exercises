53ba5ead392dd926b8c9cf39fde71bf2bd2c2d73
KirillAnastasiev <kirillanastasiev1986@gmail.com>
2025-06-27 07:45:19 +0300

    stack: LinkedStack class without realization added

---
+++ stack/src/main/java/com/bobocode/LinkedStack.java
@@ -0,0 +1,26 @@
+package com.bobocode;
+
+public class LinkedStack<T> implements Stack<T> {
+
+    @Override
+    public void push(T element) {
+        //TODO
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public T pop() {
+        //TODO
+        throw new UnsupportedOperationException();    }
+
+    @Override
+    public int size() {
+        //TODO
+        throw new UnsupportedOperationException();    }
+
+    @Override
+    public boolean isEmpty() {
+        //TODO
+        throw new UnsupportedOperationException();    }
+
+}
