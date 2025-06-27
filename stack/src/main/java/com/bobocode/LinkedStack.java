725c971b141c11af3cb270fed1f35a189c80e8f3
KirillAnastasiev <kirillanastasiev1986@gmail.com>
2025-06-27 08:47:20 +0300

    stack: LinkedStack.size() done

--- stack/src/main/java/com/bobocode/LinkedStack.java
+++ stack/src/main/java/com/bobocode/LinkedStack.java
@@ -1,22 +1,41 @@
 package com.bobocode;
 
+/**
+ *
+ * {@link LinkedStack} is a data structure that follows "last in, first out" rule (LIFO).
+ * <br/>
+ * {@link LinkedStack} implements {@link Stack} interface by linking of single generic nodes.
+ */
 public class LinkedStack<T> implements Stack<T> {
+
+    private int size;
 
     @Override
     public void push(T element) {
         //TODO
-        throw new UnsupportedOperationException();
+
+        size++;
+//        throw new UnsupportedOperationException();
     }
 
     @Override
     public T pop() {
         //TODO
-        throw new UnsupportedOperationException();    }
 
+        size--;
+
+        return null;
+    }
+
+    /**
+     * Returns a size of the stack.
+     *
+     * @return an integer value that is a size of stack
+     */
     @Override
     public int size() {
-        //TODO
-        throw new UnsupportedOperationException();    }
+        return size;
+    }
 
     @Override
     public boolean isEmpty() {
--- stack/src/test/java/com/bobocode/StackTest.java
+++ stack/src/test/java/com/bobocode/StackTest.java
@@ -1,6 +1,10 @@
 package com.bobocode;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StackTest {
 
@@ -9,6 +13,44 @@ public class StackTest {
     @BeforeEach
     void setUp() {
         stack = new LinkedStack<>();
+    }
+
+    @Test
+    @DisplayName("Test size of stack without elements")
+    public void testSizeOfStackWithoutElements() {
+        int expected = 0;
+        int actual = stack.size();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("Test size of stack after adding elements")
+    public void testSizeOfStackAfterAddingElements() {
+        stack.push(5);
+        stack.push(73);
+        stack.push(12);
+
+        int expected = 3;
+        int actual = stack.size();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("Test size of stack after popping elements")
+    public void testSizeOfStackAfterPoppingElements() {
+        stack.push(5);
+        stack.push(73);
+        stack.push(12);
+
+        stack.pop();
+        stack.pop();
+
+        int expected = 1;
+        int actual = stack.size();
+
+        assertEquals(expected, actual);
     }
 
 }
